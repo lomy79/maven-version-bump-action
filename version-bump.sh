059f0b498fabb3e7bfe1ecec1082825eadcf32b6
andrea <andrea.aresu@mycomp.it>
2023-10-07 19:58:58 +0200

    set version from properly directory

--- version-bump.sh
+++ version-bump.sh
@@ -51,7 +51,7 @@ else
   echo $BUMP_MODE "version bump detected"
   bump $BUMP_MODE $OLD_VERSION
   echo "pom.xml at" $POMPATH "will be bumped from" $OLD_VERSION "to" $NEW_VERSION
-  mvn -q versions:set -DnewVersion="${NEW_VERSION}"
+  cd $POMPATH && mvn -q versions:set -DnewVersion="${NEW_VERSION}"
   git add $POMPATH/pom.xml
   REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
   git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
